a6dac5d4b72ab886f38db0af2672d8e02cdd915b
LeoAndo <leo.777.info@gmail.com>
2025-06-01 00:57:39 +0900

    first commit

---
+++ 15/SimpleCacheRequest.java
@@ -0,0 +1,22 @@
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class SimpleCacheRequest extends CacheRequest {
+  
+  ByteArrayOutputStream out = new ByteArrayOutputStream();
+  
+  public OutputStream getBody() throws IOException {
+    return out;
+  }
+
+  public void abort() {
+    out = null; 
+  }
+
+  public byte[] getData() {
+    if (out == null) return null;
+    else return out.toByteArray();
+  }
+  
+}
